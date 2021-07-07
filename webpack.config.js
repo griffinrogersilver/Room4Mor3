77fd757167d84888e933e568139760012ff18b2b
griffinrogersilver <62974059+griffinrogersilver@users.noreply.github.com>
2021-07-06 17:08:47 -0700

    boilerplate code for Room4Mor3

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const entry = [
+    './client/index.js'
+  ];
+  
+  const output = {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  };
+  
+  module.exports = {
+    mode: process.env.NODE_ENV,
+    devServer : {
+      publicPath: '/build',
+      compress: true,
+      port: 8080,
+      hot:true
+    },
+    entry,
+    output,
+    devtool: "eval-source-map",
+    module: {
+      rules:[
+        {
+          test: /.(js|jsx)$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            }
+          }
+        },
+        {
+          test: /\.s?css/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ]
+    },
+  };    
